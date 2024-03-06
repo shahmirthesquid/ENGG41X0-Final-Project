7af948ffb36545e91516198dddd4519590d5184f
shahmirthesquid <89569533+shahmirthesquid@users.noreply.github.com>
2024-03-06 11:15:23 -0500

    test run
    got it running

---
+++ .pio/libdeps/esp12e/ESP8266 IoT Framework/src/generated/dash.h
@@ -0,0 +1,22 @@
+#ifndef DASH_H
+#define DASH_H
+
+struct dashboardData
+{
+	float dummyFloat;
+	bool Outlet1_Power;
+	bool Outlet1_Timer;
+	char projectName[64];
+	char gabe_is_poopy_head[32];
+	char duncan_is_poopy_head[32];
+	char harrod_is_poopy_head[32];
+	char shahmir_is_keeewlest[32];
+	char stringInput[32];
+	uint16_t dummyInt;
+	bool dummyBool;
+	uint16_t inputInt;
+	bool inputBool;
+	float inputFloat;
+};
+
+#endif
